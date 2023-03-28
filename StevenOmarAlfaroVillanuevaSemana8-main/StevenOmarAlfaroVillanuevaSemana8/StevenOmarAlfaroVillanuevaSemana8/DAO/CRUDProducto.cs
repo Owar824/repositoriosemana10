72c0a1563df4df76dd1393eee216a635dac15964
G3R50NP11 <125324495+Owar824@users.noreply.github.com>
2023-03-28 03:49:11 +0200

    a

---
+++ StevenOmarAlfaroVillanuevaSemana8-main/StevenOmarAlfaroVillanuevaSemana8/StevenOmarAlfaroVillanuevaSemana8/DAO/CRUDProducto.cs
@@ -0,0 +1,64 @@
+﻿using StevenOmarAlfaroVillanuevaSemana8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StevenOmarAlfaroVillanuevaSemana8.DAO
+{
+     public class CRUDProducto
+    {
+        public void AgregarProducto(Producto producto) 
+        {
+            using (AlmacenContext db = new AlmacenContext()) 
+            { 
+             Producto producto1 = new Producto();    
+              producto1.Nombre = producto.Nombre;
+              producto1.Descripcion = producto.Descripcion;
+              producto1.Precio = producto.Precio;
+              producto1.Stock = producto.Stock;
+              db.Add(producto1);
+
+              db.SaveChanges();
+
+
+            }
+        }
+
+        public Producto ProductoIndividual(int id) 
+        {
+            using (AlmacenContext db = new AlmacenContext()) 
+            { 
+              
+             var buscar =  db.Productos.FirstOrDefault(x => x.Id == id);
+
+                return buscar;
+            
+            }
+        
+        }
+
+        public void ActualizarProducto(Producto ParameterProduct)
+        {
+            using (AlmacenContext db = new AlmacenContext()) 
+            {
+             var buscar = ProductoIndividual(ParameterProduct.Id);
+                if (buscar == null)
+                {
+                    Console.WriteLine("El id no existe");
+                }
+                else 
+                { 
+                 buscar.Nombre =ParameterProduct.Nombre;
+                    db.Update(buscar);
+                    db.SaveChanges();
+                }
+            }
+        
+        }
+
+
+     }
+}
---
+++ StevenOmarAlfaroVillanuevaSemana8-main/StevenOmarAlfaroVillanuevaSemana8/StevenOmarAlfaroVillanuevaSemana8/Program.cs
@@ -0,0 +1,40 @@
+﻿using StevenOmarAlfaroVillanuevaSemana8.DAO;
+using StevenOmarAlfaroVillanuevaSemana8.Models;
+
+CRUDProducto crud = new CRUDProducto();
+Producto producto = new Producto();
+
+Console.WriteLine("Menú");
+Console.WriteLine("Pulse 1 para insertar Producto");
+Console.WriteLine("Pulse 2 para  actualizar Producto");
+var Menu = Convert.ToInt32(Console.ReadLine());
+
+switch (Menu) 
+{
+    case 1:
+        int bucle = 1;
+        while (bucle == 1) 
+        {
+            Console.WriteLine("Ingresa el nombre de tu Producto: ");
+            producto.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa la descripcion de tu producto: ");
+            producto.Descripcion = Console.ReadLine();
+            Console.WriteLine("Ingresa el precio de tu producto: ");
+            producto.Precio = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Ingresa la cantidad de producto disponible: ");
+            producto.Stock = Convert.ToInt32(Console.ReadLine());
+            crud.AgregarProducto(producto);
+
+            Console.WriteLine("El producto se ingreso corectamente");
+            Console.WriteLine("pulse 1 para continuar agregando productos");
+            Console.WriteLine("Pulsa 0 para salir");
+            bucle = Convert.ToInt32(Console.ReadLine());
+        }
+        break;
+      case 2:
+        Console.WriteLine("Actualizar datos");
+        break;
+
+
+
+}
